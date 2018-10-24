5acbc12e4a568fa4dbb9c0b5f3fa77d8263b8109
JeffreyRJohnson <jeffreyrjohnsonsemail@gmail.com>
2018-10-24 11:57:08 -0500

    Updates

---
+++ precache-manifest.085253a04295a104cb8a0131003715f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0033eed165a1875dbefa",
+    "url": "/cp-challenge-menu/static/js/runtime~main.0033eed1.js"
+  },
+  {
+    "revision": "04e12797fa2c4901595b",
+    "url": "/cp-challenge-menu/static/js/main.04e12797.chunk.js"
+  },
+  {
+    "revision": "b2fd0420134aa70449ee",
+    "url": "/cp-challenge-menu/static/js/1.b2fd0420.chunk.js"
+  },
+  {
+    "revision": "04e12797fa2c4901595b",
+    "url": "/cp-challenge-menu/static/css/main.803fa48b.chunk.css"
+  },
+  {
+    "revision": "c9d0935b01c92ba72185ce83172c1bb3",
+    "url": "/cp-challenge-menu/index.html"
+  }
+];
